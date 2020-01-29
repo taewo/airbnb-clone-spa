70fbd698b3421f1f941a79cc4f4d2bbeafaffaa4
taewo <moondingie5@gmail.com>
2020-01-29 15:49:17 +0900

    Updates

---
+++ precache-manifest.7edb999721487cb1ad04bd6142bc6719.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "606b207e17eed95a07605bdf4e805ba3",
+    "url": "/airbnb-clone-spa/index.html"
+  },
+  {
+    "revision": "39d18de9326d69f55abc",
+    "url": "/airbnb-clone-spa/static/css/main.3da55d22.chunk.css"
+  },
+  {
+    "revision": "27b11448cec569f3a973",
+    "url": "/airbnb-clone-spa/static/js/2.daf5c20a.chunk.js"
+  },
+  {
+    "revision": "b651d5533fc80509587379dd07cc06bd",
+    "url": "/airbnb-clone-spa/static/js/2.daf5c20a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "39d18de9326d69f55abc",
+    "url": "/airbnb-clone-spa/static/js/main.e77b608e.chunk.js"
+  },
+  {
+    "revision": "430855708d28cb26c984",
+    "url": "/airbnb-clone-spa/static/js/runtime-main.92223953.js"
+  }
+]);
