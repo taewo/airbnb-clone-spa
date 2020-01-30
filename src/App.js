beee6733551bba1753a1133be658f90173207dbc
taewo <moondingie5@gmail.com>
2020-01-30 18:40:51 +0900

    react-router 세팅

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import Home from './components/Home'
+import Test from './components/Test'
+
 import MainContainer from './containers/MainContainer'
+import { Link, Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="App" >
-      <MainContainer />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={MainContainer}></Route>
+        <Route path="/test" component={Test}></Route>
+      </Switch>
+    </Router>
   );
 }
 
